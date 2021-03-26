0e309172dc76aff034b638e4059158e2c6fb1e74
Zoule <zoule@eastmoney.com>
2021-03-26 14:07:13 +0800

    finish admin

--- zhihu/admin.py
+++ zhihu/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import Question, Answer
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
 # Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'create_by',)
+
+    inlines = [
+        AnswerInline
+    ]
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer_text', 'create_by',)
--- zhihu/models.py
+++ zhihu/models.py
@@ -5,14 +5,26 @@ from django.db import models
 class Question(models.Model):
     title = models.CharField(max_length=150, verbose_name="问题")
     detail = models.TextField(verbose_name="补充描述", blank=True)
-    create_by = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="提问者", editable=False)
+    create_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="questions", verbose_name="提问者")
     create_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     modify_at = models.DateTimeField(auto_now=True, verbose_name="最近修改时间")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = verbose_name_plural = "问题"
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="问题")
-    content = models.TextField(verbose_name="回答")
-    create_by = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="回答者", editable=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="问题")
+    answer_text = models.TextField(verbose_name="回答")
+    create_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="answers", verbose_name="回答者")
     create_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     modify_at = models.DateTimeField(auto_now=True, verbose_name="最近修改时间")
+
+    def __str__(self):
+        return self.answer_text
+
+    class Meta:
+        verbose_name = verbose_name_plural = "回答"
